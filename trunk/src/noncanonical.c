3709bc438c462e1db10e8948aea35def7b7275fd
gasparlafurtado@gmail.com <gasparlafurtado@gmail.com@33e02b78-6b70-32c9-ac0a-7bd24ebd7f62>
2010-10-22 15:57:26 +0000

    modificacao para funcionar com pipes. basta executar sem parametros nesta ordem: primeiro ./rece depois ./send

--- trunk/src/noncanonical.c
+++ trunk/src/noncanonical.c
@@ -11,6 +11,13 @@
 #define FALSE 0
 #define TRUE 1
 
+#define SERIAL 1
+#define PIPE 0
+
+#define FIFO_PERMS (S_IRWXU | S_IWGRP | S_IWOTH) 
+#define FIFO_READ O_RDONLY// | O_NONBLOCK
+#define FIFO_WRITE O_WRONLY
+
 #define FLAG 0x7E
 #define A_Rcv_to_Snd 0x01
 #define A_Snd_to_Rcv 0x03 
@@ -19,63 +26,88 @@
 #define C_DISC 0x0B
 
 volatile int STOP=FALSE;
+int mode;
 
 int main(int argc, char** argv)
 {
-    int fd,c, res;
+    int fd[2],c, res;
     struct termios oldtio,newtio;
     char buf[255];
 
     if ( (argc < 2) || 
   	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
-  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-      exit(1);
+  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) 
+	{
+    	/*printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+    	exit(1);*/
+		if(mkfifo("R_2_E", FIFO_PERMS)!=0)
+		{
+			printf("failed to create fifo R_2_E\n");
+			exit(1);
+		}
+		printf("criou fifo de saida\n");
+		if((fd[1]=open("R_2_E", FIFO_WRITE))==-1){
+			printf("failed to open fifo: R_2_E\n");
+			return 1;
+  		}
+		sleep(1);
+		printf("abriu fifo de saida\n");
+		if((fd[0]=open("E_2_R", FIFO_READ))==-1){
+			printf("failed to open fifo: E_2_R\n");
+			return 1;
+  		}
+		printf("abriu fifo de entrada\n");
+		mode=PIPE;
     }
+	else mode=SERIAL;
 
 
-  /*
-    Open serial port device for reading and writing and not as controlling tty
-    because we don't want to get killed if linenoise sends CTRL-C.
-  */
-  
-    
-    fd = open(argv[1], O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(argv[1]); exit(-1); }
+	if(mode==SERIAL)
+	{
+	  /*
+		Open serial port device for reading and writing and not as controlling tty
+		because we don't want to get killed if linenoise sends CTRL-C.
+	  */
+	  
+		
+		fd[0] = open(argv[1], O_RDWR | O_NOCTTY );
+		fd[1] = fd[0];
+		if (fd[0] <0) {perror(argv[1]); exit(-1); }
 
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-      perror("tcgetattr");
-      exit(-1);
-    }
+		if ( tcgetattr(fd[0],&oldtio) == -1) { /* save current port settings */
+		  perror("tcgetattr");
+		  exit(-1);
+		}
 
-    bzero(&newtio, sizeof(newtio));
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
+		bzero(&newtio, sizeof(newtio));
+		newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+		newtio.c_iflag = IGNPAR;
+		newtio.c_oflag = 0;
 
-    /* set input mode (non-canonical, no echo,...) */
-    newtio.c_lflag = 0;
+		/* set input mode (non-canonical, no echo,...) */
+		newtio.c_lflag = 0;
 
-    newtio.c_cc[VTIME]    = 5;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 0;   /* blocking read until 5 chars received */
-
-
-
-  /* 
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-    leitura do(s) próximo(s) caracter(es)
-  */
+		newtio.c_cc[VTIME]    = 5;   /* inter-character timer unused */
+		newtio.c_cc[VMIN]     = 0;   /* blocking read until 5 chars received */
 
 
 
-    tcflush(fd, TCIOFLUSH);
+	  /* 
+		VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+		leitura do(s) próximo(s) caracter(es)
+	  */
 
-    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
 
-    printf("New termios structure set\n");
+
+		tcflush(fd[0], TCIOFLUSH);
+
+		if ( tcsetattr(fd[0],TCSANOW,&newtio) == -1) {
+		  perror("tcsetattr");
+		  exit(-1);
+		}
+
+		printf("New termios structure set\n");
+	}
 
 /********** para receber uma cadeia de caracteres **************
 	int itt;
@@ -109,7 +141,7 @@ int main(int argc, char** argv)
 	while (acabou == FALSE) 
 	{
 		while (res!=1)
-        		res = read(fd,&aux,1); 
+        		res = read(fd[0],&aux,1); 
 		res=-1;
 		if(aux==FLAG)
 		{
@@ -119,20 +151,20 @@ int main(int argc, char** argv)
 			while(aux!=FLAG)
 			{
 				while (res!=1)
-					res = read(fd,&aux,1); 
+					res = read(fd[0],&aux,1); 
 				res=-1;
 				buf[itt]=aux;
 				itt++;
 			}
-			if(buf[0]==FLAG && buf[1]==A_Snd_to_Rcv && buf[2]==C_SET && buf[3]==A_Snd_to_Rcv^C_SET && buf[4]==FLAG)
+			if(buf[0]==FLAG && buf[1]==A_Snd_to_Rcv && buf[2]==C_SET && buf[3]==(A_Snd_to_Rcv^C_SET) && buf[4]==FLAG)
 			{
 				printf("recebi trama SET!\n");
 				buf[0]=FLAG;
 				buf[1]=A_Snd_to_Rcv;
 				buf[2]=C_UA;
-				buf[3]=A_Snd_to_Rcv^C_UA;
+				buf[3]=(A_Snd_to_Rcv^C_UA);
 				buf[4]=FLAG;
-				res=write(fd,buf,5);
+				res=write(fd[1],buf,5);
 				printf("enviei trama UA! com %d bytes\n", res);
 				acabou = TRUE;
 			}
@@ -148,7 +180,7 @@ int main(int argc, char** argv)
 	while(1){
 		
 		while (res!=1)
-        		res = read(fd,&aux,1); //vai lendo caracteres
+        		res = read(fd[0],&aux,1); //vai lendo caracteres
 		res=-1;
 
 		if(aux == FLAG){ //se encontrar a FLAG enquanto le os caracteres, entao vai tratar a trama
@@ -158,15 +190,15 @@ int main(int argc, char** argv)
 			while(aux!=FLAG) //enquanto nao encontrar outra FLAG (que indica que terminou a trama),  
 			{		//vai processala
 				while (res!=1)
-					res = read(fd,&aux,1); 
+					res = read(fd[0],&aux,1); 
 				res=-1;
 				buf[itt]=aux;
 				itt++;
 			}
-			if(buf[0]==FLAG && buf[1]==A_Snd_to_Rcv && buf[2]==C_DISC && buf[3]==A_Snd_to_Rcv^C_DISC && buf[4]==FLAG) //se no buffer ja esta a trama toda comecando e acabando na FLAG, entao informa que recebeu e reenvia
+			if(buf[0]==FLAG && buf[1]==A_Snd_to_Rcv && buf[2]==C_DISC && buf[3]==(A_Snd_to_Rcv^C_DISC) && buf[4]==FLAG) //se no buffer ja esta a trama toda comecando e acabando na FLAG, entao informa que recebeu e reenvia
 			{
 				printf("recebi trama DISC!\n");
-				res=write(fd,buf,5);
+				res=write(fd[1],buf,5);
 				printf("enviei trama DISC! com %d bytes\n", res);
 				break;
 			}
@@ -181,7 +213,7 @@ int main(int argc, char** argv)
 while (1) 
 	{
 		while (res!=1)
-        		res = read(fd,&aux,1); 
+        		res = read(fd[0],&aux,1); 
 		res=-1;
 		if(aux==FLAG)
 		{
@@ -191,17 +223,18 @@ while (1)
 			while(aux!=FLAG)
 			{
 				while (res!=1)
-					res = read(fd,&aux,1); 
+					res = read(fd[0],&aux,1); 
 				res=-1;
 				buf[itt]=aux;
 				itt++;
 			}
-			if(buf[0]==FLAG && buf[1]==A_Rcv_to_Snd && buf[2]==C_UA && buf[3]==A_Rcv_to_Snd^C_UA && buf[4]==FLAG)
+			if(buf[0]==FLAG && buf[1]==A_Rcv_to_Snd && buf[2]==C_UA && buf[3]==(A_Rcv_to_Snd^C_UA) && buf[4]==FLAG)
 			{
 				printf("recebi trama UA!\n");
 				printf("Conclui com êxito a transmissão de pacotes");
 				break;
 			}
+			printf("recebi trama errada\n");
 		}
 	}
 
@@ -211,9 +244,14 @@ while (1)
     O ciclo WHILE deve ser alterado de modo a respeitar o indicado no guião 
   */
 
+	if(mode==SERIAL)
+	{
+		sleep(2);
+		tcsetattr(fd[0],TCSANOW,&oldtio);
+	}
+	else close(fd[1]);
 
-    sleep(2);
-    tcsetattr(fd,TCSANOW,&oldtio);
-    close(fd);
+	close(fd[0]);
+
     return 0;
 }
